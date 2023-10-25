91262e1442869b7fe55a923213437f944f0e7276
Xarri Jorge <kodinglife@gmail.com>
2023-10-25 22:25:45 +0000

    Test contract deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,29 @@ require('dotenv').config()
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.19",
+// };
 module.exports = {
-  solidity: "0.8.19",
-};
+  // defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.WALLET_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.FINWIZ_KEY
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
